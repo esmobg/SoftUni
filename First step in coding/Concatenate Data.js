6552fcbd504e203d7991f643cf1f8ac7e1743fbf
ismail salih ismail <ismail.ismailov@gmail.com>
2022-10-27 12:24:11 +0300

    answer task

---
+++ First step in coding/Concatenate Data.js
@@ -0,0 +1,22 @@
+function concatenateData(input) {
+
+    let name = input[0]
+    let  family  = input[1]
+    let age = input [2]
+    let city = input [3]
+
+
+
+
+
+
+console.log(`You are ${name}  ${family} a ${age} old ftom ${city}`)
+
+
+
+
+
+
+
+} 
+concatenateData(['Maria', 'Ivanova' , '29' , 'Sofia'])
