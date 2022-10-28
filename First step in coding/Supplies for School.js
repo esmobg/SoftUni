fc57e7a3b18211b9d9848193eb0a21e1aa7df470
ismail salih ismail <ismail.ismailov@gmail.com>
2022-10-28 13:29:19 +0300

    task complete

---
+++ First step in coding/Supplies for School.js
@@ -0,0 +1,38 @@
+function SuppliesforSchool (input) {
+
+    let pens = Number(input[0])
+    let  markers = Number (input[1])
+    let detragent = Number (input[2])
+    let procentage = Number (input[3])
+
+
+    let sum = pens * 5.80 + markers * 7.20  + detragent * 1.2 
+    let discount = (procentage / 100) * sum 
+    let totalsum  = sum - discount 
+
+
+    console.log(totalsum)
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+SuppliesforSchool(["2 ",
+
+"3 ",
+
+"4 ",
+
+"25 "])
