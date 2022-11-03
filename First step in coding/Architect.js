037b6ece76b691732777b9e0ac055c084557733e
ismail salih ismail <ismail.ismailov@gmail.com>
2022-11-03 14:39:21 +0200

    example task complete

---
+++ First step in coding/Architect.js
@@ -0,0 +1,15 @@
+function architect(input) {
+
+    let ArchitectName = (input[0]);
+    let Project = Number(input[1]);
+
+     TimeTotal =  Project * 3 ; 
+
+    console.log(`The architect ${ArchitectName} will need ${TimeTotal} to complete 4 project/s.`)
+
+
+}
+
+architect(["Sanya",
+
+"9"]);
--- First step in coding/YardGreenings.js
+++ First step in coding/YardGreenings.js
@@ -1,12 +1,12 @@
 function yardGreening (input){
-    const squareMeters = Number(input[0]);
-    const priceperMeter = 7.61;
-    const priceWithoutDiscount = squareMeters * priceperMeter;
-    const priceWithDiscount = priceWithoutDiscount * 0.18;
-    const discount = priceWithoutDiscount - priceWithDiscount; 
+    let squareMeters = Number(input[0]);
+    let  priceperMeter = 7.61;
+    let  priceWithoutDiscount = squareMeters * priceperMeter;
+    let  priceWithDiscount = priceWithoutDiscount * 0.18;
+    let  discount = priceWithoutDiscount - priceWithDiscount; 
 
     console.log(`The final price is: ${priceWithDiscount.toFixed(2)} lv.`);
     console.log(`The discount is: ${discount.toFixed(2)} lv.`);
 }
 
-yardGreening( ["56" , "48" , "35"]  );
+yardGreening( ["550"]  );
