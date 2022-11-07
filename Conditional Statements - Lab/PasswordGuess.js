395ba329a393068fbc042a7cf2d18d1a9736396f
ismail salih ismail <ismail.ismailov@gmail.com>
2022-11-07 12:49:35 +0200

    task complete

---
+++ Conditional Statements - Lab/PasswordGuess.js
@@ -0,0 +1,21 @@
+function Password(input) {
+    let string  = input[0]
+    
+     
+
+    if ( string === "s3cr3t!P@ssw0rd" ) {
+        console.log("Welcome")
+    }
+
+    else  {
+        console.log("Wrong password!")
+
+    }
+       
+    
+    
+
+   
+}
+
+Password (["s3cr3t!P@ssw0rd"])
