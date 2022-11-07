c87c1dd09d62104d3cea61e7914e58651e164c90
ismail salih ismail <ismail.ismailov@gmail.com>
2022-11-07 11:28:49 +0200

    task complete

---
+++ Conditional Statements - Lab/GreaterNumber.js
@@ -0,0 +1,19 @@
+function GreaterNumber(input) {
+
+    let  Num1 = Number(input[0]);
+    let Num2 = Number(input[1]);
+
+    if (Num1 > Num2 ) {
+
+        console.log(Num1);
+    }
+
+    else {
+        console.log(Num2)
+    }
+
+
+
+}
+
+GreaterNumber(["10", "10"])
