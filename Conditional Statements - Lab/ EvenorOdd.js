c89d46c29456755b8004c6099a3fe96fb6082179
ismail salih ismail <ismail.ismailov@gmail.com>
2022-11-07 11:47:13 +0200

    complete task

---
+++ Conditional Statements - Lab/ EvenorOdd.js
@@ -0,0 +1,15 @@
+function EvenOrOdd (input) {
+    let Num1 = Number(input[0]);
+
+    if (Num1 % 2 === 0) {
+        console.log("even")
+    }
+
+    else {
+        console.log("odd")
+    }
+
+
+}
+
+EvenOrOdd(["1024"])
