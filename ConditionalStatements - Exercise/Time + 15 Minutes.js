82aa6335e0ecad4f279e5e55b0e8b95c420436a7
ismail salih ismail <ismail.ismailov@gmail.com>
2022-11-08 14:12:09 +0200

    example task complete

--- ConditionalStatements - Exercise/Time + 15 Minutes.js
+++ ConditionalStatements - Exercise/Time + 15 Minutes.js
@@ -1,7 +1,24 @@
-function Time(input)  {
+function time(input) {
+    let hours = Number(input[0]);
+    let mins  = Number(input[1]);
+
+    let totalTimeMins = (hours * 60) + mins;
+    let time15Min = totalTimeMins + 15;
+    let timeHour = Math.floor(time15Min / 60);
+    let timeMins = time15Min % 60;
+
     
-
+ 
+    if (timeMins < 10)  {
+        console.log(`${timeHour}:0${timeMins}`);
+    }
+    else if (timeHour > 23) {
+        timeHour = 0;
+        console.log(`${timeHour}:${timeMins}`);
+    }   else {
+        console.log(`${timeHour}:${timeMins}`);
+    }
+ 
+ 
 }
-
-Time()
-
+time(["12", "49"])
